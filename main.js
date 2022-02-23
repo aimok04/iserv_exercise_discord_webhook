d8b61322d3760f4e00dc2742b196ace0299f33d7
aimok04 <74149527+aimok04@users.noreply.github.com>
2022-02-23 02:53:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const iserv = require("./IServClient");
 
 const discord = require("discord.js");
 const https = require("https");
-const clone = require("git-clone/promise");
 const fs = require('fs-extra')
 
 const args = process.argv.slice(2);
